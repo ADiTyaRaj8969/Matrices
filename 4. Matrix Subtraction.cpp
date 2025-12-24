1dd98d16cd9bba444d8aa6ee20c173069687c0e0
Aditya <147165909+ADiTyaRaj8969@users.noreply.github.com>
2025-12-24 23:12:58 +0530

    Add files via upload

---
+++ 4. Matrix Subtraction.cpp
@@ -0,0 +1,21 @@
+// Topic: Matrix Subtraction
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<vector<int>> A = {{5, 6}, {7, 8}};
+    vector<vector<int>> B = {{1, 2}, {3, 4}};
+    int n = A.size(), m = A[0].size();
+    vector<vector<int>> C(n, vector<int>(m));
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < m; ++j) C[i][j] = A[i][j] - B[i][j];
+    cout << "Diff:\n";
+    for (auto& r : C)
+    {
+        for (auto v : r) cout << v << " ";
+        cout << "\n";
+    }
+}
+
+// TC: O(n*m)
+// SC: O(n*m)
